59b3d89c7c20cfdcc9e0e195fb3a3f5842ef0c49
Richard Myatt <lydgate77@hotmail.co.uk>
2018-04-24 20:08:17 +0100

    basic operation of light pads now established and title added

--- js/simon_game.js
+++ js/simon_game.js
@@ -1,3 +1,9 @@
+var snd1 = new Audio("audio/simonSound1.mp3");
+var snd2 = new Audio("audio/simonSound2.mp3");
+var snd3 = new Audio("audio/simonSound3.mp3");
+var snd4 = new Audio("audio/simonSound4.mp3");
+
+
 var LightPad = {
   template: "#light-pad",
   props: ["bg"]
@@ -6,6 +12,7 @@ var LightPad = {
 var main = new Vue({
   el: "#simon-app",
   data: {
+    duration: 2000,
     bg1: "#00a74a", // #2fc170
     bg2: "#9f0f17", // #c7202a
     bg3: "#cca707", // #e5c01f
@@ -15,19 +22,35 @@ var main = new Vue({
     "light-pad": LightPad
   },
   methods: {
-    handleUser: function(pad) {
+    activatePad: function(pad) {
       switch(pad) {
         case 1:
           this.bg1 = "#2fc170";
+          snd1.play();
+          setTimeout(function() {
+            main.bg1 = "#00a74a";
+          }, this.duration);
           break;
         case 2:
           this.bg2 = "#c7202a";
+          snd2.play();
+          setTimeout(function() {
+            main.bg2 = "#9f0f17";
+          }, this.duration);
           break;
         case 3:
           this.bg3 = "#e5c01f";
+          snd3.play();
+          setTimeout(function() {
+            main.bg3 = "#cca707";
+          }, this.duration);
           break;
         case 4:
           this.bg4 = "#1d6abc";
+          snd4.play();
+          setTimeout(function() {
+            main.bg4 = "#094a8f";
+          }, this.duration);
       }
     }
   }
