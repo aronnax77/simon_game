c41f224854ad533f97051ff9d56f9ca9d3c10aac
Richard Myatt <lydgate77@hotmail.co.uk>
2018-05-09 20:31:23 +0100

    game logic now appears to be working.  Need to work on start/reset button

--- js/simon_game.js
+++ js/simon_game.js
@@ -22,6 +22,8 @@ var main = new Vue({
     settingsEnabled: true,
     compEnabled: false,         // computer play enabled?
     userEnabled: false,         // user play enabled?
+    gameEnabled: false,
+    state: "Start",
     duration: 1200,             // duration of computer play
     counter: "00",
     level: 3,                   // initial value of level (change to 10)
@@ -32,18 +34,27 @@ var main = new Vue({
     bg2: "#9f0f17", // #c7202a
     bg3: "#cca707", // #e5c01f
     bg4: "#094a8f",  // #1d6abc
-    showModal: false
+    showModal: false,
+    error: false,
+    success: false
   },
   components: {
     "light-pad": LightPad
   },
   methods: {
     startGame: function() {
-      if(!this.compEnabled) {
+      if(!this.gameEnabled) {
         this.settingsEnabled = false;
         this.compEnabled = true;
+        this.gameEnabled = true;
+        this.state = "Reset";
         this.compPlay();
+      } else {
+        this.reset();
       }
+    },
+    reset: function() {
+      alert("reset");
     },
     compPlay: function() {
       var count = 1;
@@ -52,7 +63,6 @@ var main = new Vue({
       this.playSequence();
     },
     userPlay: function() {
-      alert("user play");
       this.userEnabled = true;
     },
     playSequence: function() {
@@ -124,22 +134,47 @@ var main = new Vue({
       }
     },
     handleCase: function(caseNum) {
-      if(main.compEnabled && main.nextInSequence < main.level) {
+      if(main.compEnabled && main.nextInSequence < main.counter) {
         main.nextInSequence++;
-      } else if(main.userEnabled && main.userSequence.length < main.level) {
+      } else if(main.userEnabled && main.userSequence.length < main.counter) {
         main.userSequence.push(caseNum);
+        main.checkUserChoice(caseNum);
       }
-      if(main.nextInSequence === main.level) {
+      if(main.nextInSequence == main.counter) {
         main.nextInSequence = undefined;
         main.compEnabled = false;
         main.userPlay();
       }
-      if(main.userSequence.length === main.level) {
+      if(main.userSequence.length == main.counter) {
         main.userEnabled = false;
+        if(!main.error && main.counter < main.level) {
+          main.setCounter(parseInt(main.counter, 10) + 1);
+          main.compEnabled = true;
+          setTimeout(function() {
+            main.playSequence();
+            main.userSequence = [];
+          }, 1200);
+        }
+      }
+      if(main.userSequence.length === main.level) {
+        main.success = true;
       }
     },
-    checkUserChoice: function() {
-      
+    checkUserChoice: function(num) {
+      if(num !== main.gameSequence[main.userSequence.length - 1]) {
+        main.error = true;
+        main.userEnabled = false;
+        main.compEnabled = true;
+        setTimeout(function() {
+          main.playSequence();
+          main.userEnabled = true;
+          main.userSequence = [];
+        }, 1500);
+      } else {
+        if(main.error) {
+          main.error = false;
+        }
+      }
     }
   },
   watch: {
@@ -148,13 +183,3 @@ var main = new Vue({
     }
   }
 });
-
-
-function sleep(millis) {
-    var date = Date.now();
-    var curDate = null;
-    do {
-        curDate = Date.now();
-    } while (curDate-date < millis);
-}
-//snd6.play();
